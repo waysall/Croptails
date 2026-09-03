648b227093f11108b48ac7a09d9f8f1b0b090ae0
waysall <nev1n@proton.me>
2026-09-03 17:04:18 +0100

    Idle State and Gameinputevents script

---
+++ scenes/characters/player/idle_state.gd
@@ -0,0 +1,36 @@
+extends NodeState
+
+@export var Player: CharacterBody2D
+@export var animated_sprite_2d: AnimatedSprite2D
+
+@export var direction: Vector2
+
+func _on_process(_delta : float) -> void:
+	pass
+
+
+func _on_physics_process(_delta : float) -> void:
+	direction = GameInputEvents.movement_input()
+	
+	if direction == Vector2.UP:
+		animated_sprite_2d.play("idle_back")
+	elif direction == Vector2.RIGHT:
+		animated_sprite_2d.play("idle_right")
+	elif direction == Vector2.DOWN:
+		animated_sprite_2d.play("idle_front")
+	elif direction == Vector2.LEFT:
+		animated_sprite_2d.play("idle_left")
+	else:
+		animated_sprite_2d.play("idle_front")
+
+
+func _on_next_transitions() -> void:
+	pass
+
+
+func _on_enter() -> void:
+	pass
+
+
+func _on_exit() -> void:
+	pass
