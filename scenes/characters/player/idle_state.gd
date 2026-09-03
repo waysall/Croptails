e766b5c4ee8a1a45201c84ef6a6a0ce82ce9d57e
waysall <nev1n@proton.me>
2026-09-03 17:33:46 +0100

    Walk State and Transitions

--- scenes/characters/player/idle_state.gd
+++ scenes/characters/player/idle_state.gd
@@ -1,31 +1,30 @@
 extends NodeState
 
-@export var Player: CharacterBody2D
+@export var player: Player
 @export var animated_sprite_2d: AnimatedSprite2D
-
-@export var direction: Vector2
 
 func _on_process(_delta : float) -> void:
 	pass
 
 
-func _on_physics_process(_delta : float) -> void:
-	direction = GameInputEvents.movement_input()
-	
-	if direction == Vector2.UP:
+func _on_physics_process(_delta : float) -> void:	
+	if player.player_direction == Vector2.UP:
 		animated_sprite_2d.play("idle_back")
-	elif direction == Vector2.RIGHT:
+	elif player.player_direction == Vector2.RIGHT:
 		animated_sprite_2d.play("idle_right")
-	elif direction == Vector2.DOWN:
+	elif player.player_direction == Vector2.DOWN:
 		animated_sprite_2d.play("idle_front")
-	elif direction == Vector2.LEFT:
+	elif player.player_direction == Vector2.LEFT:
 		animated_sprite_2d.play("idle_left")
 	else:
 		animated_sprite_2d.play("idle_front")
 
 
 func _on_next_transitions() -> void:
-	pass
+	GameInputEvents.movement_input()
+	
+	if GameInputEvents.is_movement_input():
+		transition.emit("Walk")
 
 
 func _on_enter() -> void:
@@ -33,4 +32,4 @@ func _on_enter() -> void:
 
 
 func _on_exit() -> void:
-	pass
+	animated_sprite_2d.stop()
