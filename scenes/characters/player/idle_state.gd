a002edf54aa96afdf7d0bfca0721207c85557803
waysall <nev1n@proton.me>
2026-09-04 20:45:32 +0100

    Player State Machine Chopping, Tilling and Watering Tool States

--- scenes/characters/player/idle_state.gd
+++ scenes/characters/player/idle_state.gd
@@ -25,6 +25,15 @@ func _on_next_transitions() -> void:
 	
 	if GameInputEvents.is_movement_input():
 		transition.emit("Walk")
+	
+	if player.current_tool == DataType.Tools.AxeWood && GameInputEvents.use_tool():
+		transition.emit("Chopping")
+	
+	if player.current_tool == DataType.Tools.TillGround && GameInputEvents.use_tool():
+		transition.emit("Tilling")
+	
+	if player.current_tool == DataType.Tools.WaterCrops && GameInputEvents.use_tool():
+		transition.emit("Watering")
 
 
 func _on_enter() -> void:
--- scenes/characters/player/player.gd
+++ scenes/characters/player/player.gd
@@ -1,4 +1,6 @@
 class_name Player
 extends CharacterBody2D
 
+@export var current_tool: DataType.Tools = DataType.Tools.None
+
 var player_direction: Vector2
